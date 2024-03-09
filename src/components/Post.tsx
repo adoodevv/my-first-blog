7822c6189e38f9c7978a70fb609777fac64c511c
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-03-09 21:29:11 +0000

    Add tailwind css

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -9,11 +9,11 @@ interface PostProps {
 
 const Post: React.FC<PostProps> = ({ title, author, excerpt, content }) => {
    return (
-      <div>
-         <h2>{title}</h2>
-         <h4>By {author}</h4>
-         <p>{excerpt}</p>
-         <p>{content}</p>
+      <div className="p-4 bg-white rounded shadow-md">
+         <h2 className="text-2xl font-bold mb-2">{title}</h2>
+         <h4 className="text-lg text-gray-600 mb-4">By {author}</h4>
+         <p className="text-gray-800 mb-4">{excerpt}</p>
+         <p className="text-gray-700">{content}</p>
       </div>
    );
 };
